582667e0f286fea1b786888237ded201c7df9aa5
Rodrigo Henrich <rodrigohenrich@gmail.com>
2025-06-05 14:21:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,14 @@
 import streamlit as st
-from json import loads
 import pandas as pd
+#Maneira rápida de criar e compartilhar aplicativos de dados
+#Falar sobre o streamlit hello
 
+#Imprime uma mensagem na tela
+st.title('Título da aplicação')
 
-st.title('Aplicativo para exibir diferentes arquivos')
+#Escrevendo texto na tela
+st.write('Texto na tela')
 
-st.markdown(':smirk:')
+#Adicionando uma imagem
 
-arquivo = st.file_uploader(
-  'Escolha um arquivo que vou tentar ler',
-  type=['png','jpg','csv','mp3','py','json', 'mp4'])
-if arquivo:
-  match arquivo.type.split('/'):
-    case 'application','json':
-      st.json(loads(arquivo.read()))
-    case 'image',_:
-      st.image(arquivo.read(),width=200)
-    case 'text','csv':
-      df = pd.read_csv(arquivo)
-      st.dataframe(df)
-    case 'text','x-python':
-      st.code(arquivo.read().decode)
-    case 'audio', _:
-      st.audio(arquivo)
-    case 'video', _:
-      st.video(arquivo)
-else:
-  st.error('Ainda não tenho arquivo')
+st.image('https://placehold.co/600x400')
